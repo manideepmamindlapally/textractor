d75b638f2292c9e58e7704c2f97b8506d9a3af91
Manideep Mamindlapally <manideepyx@gmail.com>
2019-03-21 15:38:51 +0530

    Remove an extra app.py file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from PIL import Image
 from backend.get_text import textrecog
 
-image_url = "https://i.ytimg.com/vi/Mh9-NlRGA7g/maxresdefault.jpg"
+image_url = "http://www.golbazaar.com/wp-content/uploads/2018/07/Mens-Opulent-Printed-T-Shirts-Vol-2-white-Printed-words-single.jpg"
 
 mode = "Handwritten"
 
 image, text = textrecog(image_url, mode)
 
 print(text)
-image.show()
+image.save("./../a.png")
